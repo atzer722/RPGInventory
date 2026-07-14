a8acf436c293566a2962d9cb6ee3fe39b5174c8b
atzer722 <atzer722@gmail.com>
2026-07-14 14:56:33 +0200

    Implement database on main class.

--- src/main/java/com/atzer/RPGInventory.java
+++ src/main/java/com/atzer/RPGInventory.java
@@ -1,6 +1,7 @@
 package com.atzer;
 
 import com.atzer.core.config.Config;
+import com.atzer.core.database.Database;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,12 +12,16 @@ public final class RPGInventory extends JavaPlugin {
     private static RPGInventory instance;
 
     private Config pluginConfig;
+    private Database database;
 
     @Override
     public void onEnable() {
         instance = this;
         this.pluginConfig = new Config();
         this.saveDefaultConfig();
+
+        this.database = new Database();
+        this.database.init();
 
         this.getLogger().info("Plugin RPGInventory enabled!");
     }
